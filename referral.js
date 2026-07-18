c2569ddeb23a51204be6d7f10d23ebd51ca6c84c
premarnuphap <nickhappy9999@gmail.com>
2026-07-18 10:26:55 +0700

    Update referral.js

--- referral.js
+++ referral.js
@@ -7,34 +7,56 @@ router.get("/:province", async (req, res) => {
     const province = req.params.province;
 
     res.send(`
-        <html>
+<!DOCTYPE html>
 
-        <head>
-            <title>เพิ่มเพื่อน</title>
-        </head>
+<html>
 
-        <body style="font-family:sans-serif;text-align:center;padding-top:80px">
+<head>
 
-            <h2>จังหวัด ${province}</h2>
+<meta charset="UTF-8">
 
-            <p>กดปุ่มด้านล่างเพื่อเพิ่มเพื่อน LINE</p>
+<title>AgriPredict</title>
 
-            <a href="https://line.me/R/ti/p/@YOUR_LINE_ID">
-                <button
-                    style="
-                        font-size:22px;
-                        padding:15px 35px;
-                        cursor:pointer;
-                    "
-                >
-                    เพิ่มเพื่อน LINE
-                </button>
-            </a>
+<script src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
 
-        </body>
+</head>
 
-        </html>
-    );
+<body style="font-family:sans-serif;text-align:center;padding-top:80px">
+
+<h2>จังหวัด ${province}</h2>
+
+<p>กำลังเชื่อมต่อ...</p>
+
+<script>
+
+async function start(){
+
+    await liff.init({
+        liffId:"2009633390-g8hIM7oO"
+    });
+
+    if(!liff.isLoggedIn()){
+
+        liff.login();
+
+        return;
+
+    }
+
+    const profile = await liff.getProfile();
+
+    console.log(profile);
+
+}
+
+start();
+
+</script>
+
+</body>
+
+</html>
+`);
 
 });
 
